4610ba0bbb11f29687b6f20b07355fd8405d9d49
shaguftaYaqub711 <bcs.f17.A.15@gmail.com>
2020-10-17 08:07:38 -0700

    Initial commit

---
+++ Controllers/productsController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApi23.Models;
+
+namespace TodoApi23.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class productsController : ControllerBase
+    {
+        private readonly productContext _context;
+
+        public productsController(productContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/products
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<product>>> GetTodoItems()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+
+        // GET: api/products/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<product>> Getproduct(long id)
+        {
+            var product = await _context.TodoItems.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+
+        // PUT: api/products/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Putproduct(long id, product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!productExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/products
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<product>> Postproduct(product product)
+        {
+            _context.TodoItems.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("Getproduct", new { id = product.Id }, product);
+        }
+
+        // DELETE: api/products/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<product>> Deleteproduct(long id)
+        {
+            var product = await _context.TodoItems.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return product;
+        }
+
+        private bool productExists(long id)
+        {
+            return _context.TodoItems.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Models/productcontext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi23.Models
+{
+    public class productContext : DbContext
+    {
+        public productContext(DbContextOptions<productContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<product> TodoItems { get; set; }
+    }
+}
